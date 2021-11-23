52734c309b852ff2801c04fe142d96d88c6e7a12
Eve Kozina <eve.kozina@gmail.com>
2021-11-23 18:38:45 -0500

    commit tree 4180c94a13dbbcfbfaa5898cb0a4741787321816

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,16 @@
 // Code Keypad Component Here
+import React from "react";
+
+function Keypad() {
+    function handleChange() {
+        console.log ("Entering password...")
+    }
+    return (
+        <input 
+            input type = "password"
+            onChange={handleChange}
+        ></input>
+    )
+}
+
+export default Keypad;
